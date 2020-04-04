dc98e02a658114bfe126b74010a4773db5d87688
cg1101 <cg1101@gmail.com>
2020-04-04 15:20:13 +1100

    add database table versions

--- db/schema.py
+++ db/schema.py
@@ -16,6 +16,16 @@ t_merchandises = sa.Table('merchandises', metadata,
     mysql_charset='utf8',
 )
 
+t_tenants = sa.Table('tenants', metadata,
+    sa.Column('tenant_id', INT(11), primary_key=True, autoincrement=True),
+    sa.Column('name', VARCHAR(255), nullable=True),
+    sa.Column('description', VARCHAR(255), nullable=True),
+    sa.Column('admin_email', VARCHAR(255), nullable=True),
+    mysql_engine='InnoDB',
+    mysql_charset='utf8',
+)
+
+
 ##########################################################################
 
 
